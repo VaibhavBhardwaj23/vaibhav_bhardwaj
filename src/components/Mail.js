e7c87bcb3325403eec78adb15ad1241dd8671ff4
VaibhavBhardwaj23 <majesty.v2507@gmail.com>
2023-05-09 14:54:19 +0530

    mail button

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Mail.css";
 const Mail = () => {
   return (
-    <div className="mail_button">
-      <span>Drop me a Line</span>
-      <a href="mailto:vaibhavb0203@icloud.com.com">
-        <img src="./images/arrow.png" alt="arrow" />
-      </a>
-    </div>
+    <a href="mailto:vaibhavb0203@icloud.com.com">
+      <div className="mail_button">
+        <span>Drop me a Line</span>
+        <a href="mailto:vaibhavb0203@icloud.com.com">
+          <img src="./images/arrow.png" alt="arrow" />
+        </a>
+      </div>
+    </a>
   );
 };
 
